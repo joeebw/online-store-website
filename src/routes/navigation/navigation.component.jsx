835b9f9f36a27edf1d0c073ec65689e1a70edeab
joeebw <joeegaarciia@gmail.com>
2022-10-21 16:34:27 -0500

    adding redux selector for user reducer

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,19 @@
 import { async } from "@firebase/util";
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import {ReactComponent as CrwnLogo} from '../../assets/wolf.svg';
 import Carticon from "../../components/cart-icon/cart-icon.component";
 import CartDropDown from "../../components/cart-dropdown/cart-dropdown.component";
-import { UserContext } from "../../contexts/user.contexts";
 import { CartContext } from "../../contexts/cart.context";
+import {selectCurrentUser} from '../../store/user/user.selector'
+
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-import {NavigationContainer, NavLink, NavLinks, LogoContainer} from './navigation.styles.jsx'
+import {NavigationContainer, NavLink, NavLinks, LogoContainer} from './navigation.styles.jsx';
+
 
 const Navigation = () => {
-
-  const {currentUser} = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const {isCartOpen} = useContext(CartContext);
 
     return(
