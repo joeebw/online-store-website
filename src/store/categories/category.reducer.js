9cb361d040815d76ed3a89141a3350fa04b2bb3a
joeebw <joeegaarciia@gmail.com>
2022-10-26 12:41:56 -0500

    performance redux-thunk and adding spinner loading

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,7 +1,9 @@
 import { CATEGORIES_ACTION_TYPES } from "./category.type";
 
 export const CATEGORY_INITIAL_STATE = {
-  categories: []  
+  categories: [],
+  isLoading: false,
+  error: null  
 }
 
 export const categoriesReducer = (state = CATEGORY_INITIAL_STATE, action = {}) => {
@@ -9,8 +11,12 @@ export const categoriesReducer = (state = CATEGORY_INITIAL_STATE, action = {}) =
     const {type, payload} = action;
 
     switch(type) {
-        case CATEGORIES_ACTION_TYPES.SET_CATEGORIES :
-            return {...state, categories: payload};
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START :
+            return {...state, isLoading: true};
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS :
+            return {...state, categories: payload, isLoading: false};
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED :
+            return {...state, error: payload, isLoading: false};
         default: 
             return state;
     }
--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -23,3 +23,7 @@ export const selectCategoriesMap = createSelector(
     )}
 )
 
+export const selectCategoriesIsLoading = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.isLoading
+);
